78b51326012a50378a486bc4366ef3be91b30029
Jai Prakash Singh <prakash.jaiisingh@gmail.com>
2026-07-29 23:34:54 +0530

    Delete src directory

--- src/utils/scrollUtils.js
+++
@@ -1,6 +0,0 @@
-export const smoothScroll = (sectionId) => {
-  const element = document.getElementById(sectionId);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-};
